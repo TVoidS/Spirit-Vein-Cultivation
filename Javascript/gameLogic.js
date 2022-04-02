7eb67d90195bbdaa9352bf36b7039290a4ebe3fc
TVoidS <werme3@gmail.com>
2022-04-02 18:16:08 -0400

    QUESTS WORK, DISPLAY, REWARD, AND ARE *ALMOST* CLEAN

--- Javascript/gameLogic.js
+++ Javascript/gameLogic.js
@@ -51,7 +51,7 @@ const game = {
 
         // Start the game loop! (KEEP AT THE END OF INIT())
         game.gameLoop= setInterval(game.gameLogic, timeStep);
-        game.questLoop = setInterval(quests.checkUnlocks, (game.baseTime * 2000))
+        game.questLoop = setInterval(quests.checkQuests, (10000)); 
     },
 
     // Primary GameLoop function!  This will repeate forever and eever and eeeeeeverrrr
@@ -852,17 +852,15 @@ const inspection = {
 }
 
 const quests = {
-    checkUnlocks: function() {
+    checkQuests: function() {
         let keys = Object.keys(quests.q);
 
         // Check each quest...
         keys.forEach(quest => {
             let newQuest = true;
-            // If the player doesn't already ave the quest...
+            // If the player doesn't already have the quest...
             if(!character.sheet.quest.hasOwnProperty(quest)) {
-
-                // Check what prereqs exist
-                if(quests.q[quest].prereqs.stats && quests.q[quest].prereqs.inventory && quests.q[quest].prereqs.tracking) {
+                if(quests.q[quest].prereqs.stats) {
                     // Then verify that the player meets the prereqs!
 
                     for (const [key, value] of Object.entries(quests.q[quest].prereqs.stats)) {
@@ -871,19 +869,8 @@ const quests = {
                             break;
                         }
                     }
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.inventory)) {
-                        if (character.sheet.inventory[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.tracking)) {
-                        if (character.sheet.tracking[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                } else if(quests.q[quest].prereqs.inventory && quests.q[quest].prereqs.tracking) {
+                }
+                if(quests.q[quest].prereqs.inventory && newQuest) {
                     // Then verify that the player meets the prereqs!
 
                     for (const [key, value] of Object.entries(quests.q[quest].prereqs.inventory)) {
@@ -892,65 +879,12 @@ const quests = {
                             break;
                         }
                     }
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.tracking)) {
-                        if (character.sheet.tracking[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                } else if(quests.q[quest].prereqs.stats && quests.q[quest].prereqs.tracking) {
-                    // Then verify that the player meets the prereqs!
-
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.stats)) {
-                        if (character.sheet.stats[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.tracking)) {
-                        if (character.sheet.tracking[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                } else if(quests.q[quest].prereqs.stats && quests.q[quest].prereqs.inventory) {
-                    // Then verify that the player meets the prereqs!
-
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.stats)) {
-                        if (character.sheet.stats[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.inventory)) {
-                        if (character.sheet.inventory[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                } else if(quests.q[quest].prereqs.tracking) {
+                }
+                if(quests.q[quest].prereqs.tracking && newQuest) {
                     // Then verify that the player meets the prereqs!
 
                     for (const [key, value] of Object.entries(quests.q[quest].prereqs.tracking)) {
                         if (character.sheet.tracking[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                } else if(quests.q[quest].prereqs.inventory) {
-                    // Then verify that the player meets the prereqs!
-
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.inventory)) {
-                        if (character.sheet.inventory[key] < value) {
-                            newQuest=false;
-                            break;
-                        }
-                    }
-                } else if(quests.q[quest].prereqs.stats) {
-                    // Then verify that the player meets the prereqs!
-
-                    for (const [key, value] of Object.entries(quests.q[quest].prereqs.stats)) {
-                        if (character.sheet.stats[key] < value) {
                             newQuest=false;
                             break;
                         }
@@ -963,7 +897,65 @@ const quests = {
                     character.sheet.quest[quest] = quests.q[quest];
                     character.sheet.quest[quest].status = "active";
                     game.updateQuestDisplay(quest);
+
+                    if(game.qdExpanded) {
+                        game.toggleQuestD();
+                    }
                 }
+            } else if (character.sheet.quest[quest].status == 'active') {
+                // If we do, then see if we are eligible for the rewards!
+                let passed = true;
+                if(quests.q[quest].requirements.stats) {
+                    for(const [key, value] of Object.entries(quests.q[quest].requirements.stats)) {
+                        if(character.sheet.stats[key] < value) {
+                            passed = false;
+                        }
+                    }
+                }
+                if(quests.q[quest].requirements.tracking) {
+                    for(const [key, value] of Object.entries(quests.q[quest].requirements.tracking)) {
+                        if(character.sheet.tracking[key] < value) {
+                            passed = false;
+                        }
+                    }
+                }
+                if(quests.q[quest].requirements.inventory) {
+                    for(const [key, value] of Object.entries(quests.q[quest].requirements.inventory)) {
+                        if(character.sheet.inventory[key] < value) {
+                            passed = false;
+                        }
+                    }
+                }
+                if (passed) {
+                    // we passed the quest! Time to apply the rewards and mark as complete
+                    character.sheet.quest[quest].status = 'Complete';
+
+                    // Fill our inventory with the new stuff!
+                    if(quests.q[quest].rewards.inventory) {
+                        for(const [key, value] of Object.entries(quests.q[quest].rewards.inventory)) {
+                            character.sheet.inventory[key] += value;
+                        }
+                        game.updateInventoryCounts();
+                    }
+
+                    // Add our stats
+                    if(quests.q[quest].rewards.stats) {
+                        for(const [key, value] of Object.entries(quests.q[quest].rewards.stats)) {
+                            character.sheet.stats[key] += value;
+                        }
+                        game.updateCharacterStatDisplays();
+                    }
+
+                    // Skills? Modules? TODO, add those rewards!
+
+                    game.updateQuestDisplay();
+                    game.registerEvent("Quest Complete", "You just completed [" + quest.q[quest].display_name + "]!");
+                    if(game.qdExpanded) {
+                        game.toggleQuestD();
+                    }
+                }
+            } else {
+                // NOTHING, cause the quest isn't active anymore!
             }
         });
     },
